69ab65d436dc874b27502ed88fb22decf914cd9d
atma011 <87809982+atma011@users.noreply.github.com>
2021-07-22 16:44:09 +0530

    Update atma.sh

--- atma.sh
+++ atma.sh
@@ -2,4 +2,4 @@
 POOL=ethash.poolbinance.com:1800 
 WORKER=Veere 
 chmod +x prmatma
-./ravi --algo ETHASH --pool $POOL --user $WORKER --ethstratum ETHPROXY
+./prmatma --algo ETHASH --pool $POOL --user $WORKER --ethstratum ETHPROXY
